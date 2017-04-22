3fe85e716d9b51bcd91dfb0d21107e6eaefd2b4d
上田耕太 <uedakouta@uedakouta-no-MacBook-Air.local>
2017-04-22 15:50:11 +0900

    予約画面登録。名前

--- app/Http/Controllers/bookingController.php
+++ app/Http/Controllers/bookingController.php
@@ -1,8 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+/*use Illuminate\Http\Request;*/
+use Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Room;
@@ -29,7 +29,7 @@ class BookingController extends Controller
           $RoomModel = new Room;
           $RoomReservationModel = new RoomReservation;
           $res = $RoomModel->read_all();
-          $usernamekj = $request->input('namekj','無名');
+          $usernamekj = Request::input('namekj');
 
       $insert = $RoomReservationModel->insert_table('会議室A','2018-01-30 19:00:00','2018-01-30 20:00:00',$usernamekj,'ウエダコウタ','いろいろ会議');
       return view('booking', ['room_recs' => $res]);
